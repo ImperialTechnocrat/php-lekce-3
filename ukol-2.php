69eae78b913763045bf5285a8d4c7f8e38ff4687
ImperialTechnocrat <juan.denagykallo@gmail.com>
2019-03-06 03:24:50 +0100

    ukol5

--- ukol-2.php
+++ ukol-2.php
@@ -21,14 +21,14 @@
         $radek = 1;
         $pocetRadku = 5;
         while ($radek <= $pocetRadku) {
+            echo "<tr>
+                <td>$radek</td>
+                <td>$pocetRadku</td>
+            </tr>";
             $radek = $radek + 1;
-                }
+        }
         ?>
-            <tr>
-                <td><?php echo ("$radek"); ?></td>
-                <td><?php echo ("$pocetRadku"); ?></td>
-            </tr>
-        <?php $radek = $radek + 1; ?>
+
         </tbody>
     </table>
 </div>
